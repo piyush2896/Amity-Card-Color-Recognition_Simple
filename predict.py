17e1eb24e22c83049039400e88d3cca7aaeb7fd1
Piyush Malhotra <piyushmalhotra28@gmail.com>
2017-12-04 21:59:40 +0530

    Add Main file

---
+++ predict.py
@@ -0,0 +1,96 @@
+import numpy as np
+import cv2
+from classifier import KNN
+import matplotlib.pyplot as plt
+import os
+
+
+def get_data(sub_folders):
+    path = './datasets/'
+    imgs = []
+    labels = []
+    for folder in sub_folders:
+        cur_folder = path + folder
+        img_list = os.listdir(cur_folder)
+        for img_name in img_list:
+            img = cv2.imread(cur_folder + '/' + img_name)
+            img = cv2.resize(img, (400, 250))
+            img = img.reshape(400 * 250 * 3,)
+            imgs.append(img)
+            labels.append(folder)
+    imgs = np.asarray(imgs)
+    labels = np.asarray(labels)
+    return imgs, labels
+
+
+def get_classifier(X_train, Y_train, k=3):
+    knn = KNN(3)
+    knn.fit(X_train, Y_train)
+    return knn
+
+
+def get_data_():
+    # get data
+    X, Y = get_data(['Orange', 'Green'])
+    Y_ = Y == 'Green'
+    Y_ = Y_.astype('float32')
+
+    # shuffle data
+    all_data = np.zeros((X.shape[0], X.shape[1] + 1))
+    all_data[:, :-1] = X
+    all_data[:, -1] = Y_
+    np.random.shuffle(all_data)
+
+    # split into X and Y
+    X, Y_ = all_data[:, :-1], all_data[:, -1]
+
+    return X, Y_
+
+
+def run(img):
+    # get data
+    X, Y_ = get_data_()
+
+    # get classifier
+    knn = get_classifier(X, Y_)
+
+    # get image
+    img = cv2.resize(img, (400, 250))
+    img = img.reshape(400 * 250 * 3,)
+
+    # predict
+    pred = knn.predict(np.array([img]))
+    return 'Orange' if pred == 0 else 'Green'
+
+
+def check_run():
+    # get data
+    X, Y = get_data(['Orange', 'Green'])
+    Y_ = Y == 'Green'
+    Y_ = Y_.astype('float32')
+
+    # shuffle data
+    all_data = np.zeros((X.shape[0], X.shape[1] + 1))
+    all_data[:, :-1] = X
+    all_data[:, -1] = Y_
+    np.random.shuffle(all_data)
+
+    # split into train and test
+    X, Y_ = all_data[:, :-1], all_data[:, -1]
+    split = int(0.9 * all_data.shape[0])
+    X_train, X_test = X[:split, :], X[split:, :]
+    Y_train, Y_test = Y_[:split], Y_[split:]
+    
+    # make a classifier
+    knn = get_classifier(X_train, Y_train)
+    return knn.accuracy(X_test, Y_test)
+
+
+if __name__ == '__main__':
+    print(check_run())
+    file = input('Enter File location: ')
+    img = cv2.imread(file)
+    img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img1)
+    plt.show()
+    print(run(img))
